f1fd5d3c657fb3cc01201f6dd6f178dfc1645266
Pavlos Rorris <159206339+PavlosR@users.noreply.github.com>
2026-05-25 13:44:14 +1000

    .

---
+++ Assets/Scripts/Managers/CutsceneManager.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class CutsceneManager : MonoBehaviour
+{
+
+    public void Play(MonoBehaviour script)
+    {
+        script.enabled = true;
+    }
+
+    public void Stop(MonoBehaviour script)
+    {
+        script.enabled = false;
+    }
+}
--- Assets/Scripts/Player Scripts/PlayerController.cs
+++ Assets/Scripts/Player Scripts/PlayerController.cs
@@ -3,6 +3,9 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
 
+    [Header("Input Manager")]
+    [SerializeField] private InputManagerPlayer inputMan;
+
     private Rigidbody2D rb;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Awake()
@@ -12,6 +15,11 @@ public class PlayerController : MonoBehaviour
 
     // Update is called once per frame
     void Update()
+    {
+        
+    }
+
+    public void Jump()
     {
         Debug.Log("Jump");
         rb.AddForce(new Vector3(0, 10, 0));
